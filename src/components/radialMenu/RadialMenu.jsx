1fe664292928da5347b8cee619460d2db3541b9d
Arad-Hb <habashiarad@gmail.com>
2025-03-04 19:30:28 +0330

    add nightMode and lightMode buttons in radial menu and modify it

--- src/components/radialMenu/RadialMenu.jsx
+++ src/components/radialMenu/RadialMenu.jsx
@@ -1,18 +1,37 @@
+'use client'
 import React from 'react'
 import styles from './RadialMenu.module.css'
-import LanguageSwitcher from '../buttons/LanguageSwitcher'
+import { setUserLocale } from '@/services/locale';
+import { usePathname, useRouter } from 'next/navigation'; 
+
 
 const RadialMenu = () => {
+  const router = useRouter();  
+  const pathname = usePathname();
+  const currentLocale = pathname.split('/')[1] || 'en';
+
+  const changeLanguage =(lang) => {  
+    if(currentLocale !== lang)
+    {
+      setUserLocale(lang);
+      router.push( `/${lang}`); 
+    }
+  };
+  const ModeHandler=(mode)=>{
+    var element = document.body;
+    element.classList.toggle(mode);
+}
+
   return (
     <div className={`${styles.container}`}>
         <span className={`${styles.wraper}`}><span></span></span>
         <div className={`${styles.wrap}`}>
+         <a href="#"><div onClick={() => changeLanguage('ir')} key="iranLocaleButton"></div></a>
+         <a href="#"><div onClick={() => changeLanguage('en')} key="englishLocaleButton"></div></a>
+         <a href="#"><div onClick={() => ModeHandler('light-mode')} key="lightModeButton"></div></a>
+         <a href="#"><div onClick={() => ModeHandler('night-mode')} key="nightModeButton"></div></a>
          <a href="#"><div></div></a>
-         <a href="#"><div></div></a>
-         <a href="#"><div></div></a>
-         <a href="#"><div></div></a>
-         <a href="#"><div></div></a>
-        </div>
+        </div> 
     </div>
   )
 }
